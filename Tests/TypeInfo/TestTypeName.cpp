077e43d1ca408bc3c605faefa50dedf289154ce3
Jon Christen <jon@entropysoftware.io>
2025-05-19 09:30:00 -0700

    Add test case for function pointer type names

--- Tests/TypeInfo/TestTypeName.cpp
+++ Tests/TypeInfo/TestTypeName.cpp
@@ -6,6 +6,26 @@
 #include "Entropy/Reflection.h"
 #include "TestMacros.h"
 
+namespace MyNamespace
+{
+
+const float& MyFunction(const Entropy::int64& x, double y);
+
+struct MyType
+{
+    const char* MyMethod(const Entropy::int64& x, MyType* y);
+    const char* MyConstMethod(const Entropy::int64& x, MyType* y) const;
+};
+
+template <typename T1, typename T2>
+struct MyTemplate
+{
+};
+
+} // namespace MyNamespace
+
+//---------------
+
 template <typename T>
 bool CheckTypeName(const std::string& expectedValue)
 {
@@ -29,6 +49,8 @@ bool CheckTypeName(const std::string& expectedValue)
 
 int TypeInfo_TestTypeName(int argc, char** const argv)
 {
+    using namespace MyNamespace;
+
     ENTROPY_VERIFY_FUNC(CheckTypeName<bool>("bool"));
     ENTROPY_VERIFY_FUNC(CheckTypeName<char>("char"));
     ENTROPY_VERIFY_FUNC(CheckTypeName<unsigned char>("unsigned char"));
@@ -38,9 +60,23 @@ int TypeInfo_TestTypeName(int argc, char** const argv)
     ENTROPY_VERIFY_FUNC(CheckTypeName<unsigned int>("unsigned int"));
     ENTROPY_VERIFY_FUNC(CheckTypeName<long long>("__int64"));
     ENTROPY_VERIFY_FUNC(CheckTypeName<unsigned long long>("unsigned __int64"));
+    ENTROPY_VERIFY_FUNC(CheckTypeName<float>("float"));
+    ENTROPY_VERIFY_FUNC(CheckTypeName<double>("double"));
 
     ENTROPY_VERIFY_FUNC(CheckTypeName<const char*>("char const*"));
     ENTROPY_VERIFY_FUNC(CheckTypeName<const char*&>("char const*&"));
+
+    ENTROPY_VERIFY_FUNC(CheckTypeName<MyType>("MyNamespace::MyType"));
+    ENTROPY_VERIFY_FUNC(CheckTypeName<MyType&>("MyNamespace::MyType&"));
+
+    ENTROPY_VERIFY_FUNC(CheckTypeName<MyTemplate<float&, const MyType*>>(
+        "MyNamespace::MyTemplate<float&, MyNamespace::MyType const*>"));
+
+    ENTROPY_VERIFY_FUNC(CheckTypeName<decltype(&MyFunction)>("float const& (*)(__int64 const&, double)"));
+    ENTROPY_VERIFY_FUNC(CheckTypeName<decltype(&MyType::MyMethod)>(
+        "char const* (MyNamespace::MyType::*)(__int64 const&, MyNamespace::MyType*)"));
+    ENTROPY_VERIFY_FUNC(CheckTypeName<decltype(&MyType::MyConstMethod)>(
+        "char const* (MyNamespace::MyType::*)(__int64 const&, MyNamespace::MyType*) const"));
 
     return 0;
 }
